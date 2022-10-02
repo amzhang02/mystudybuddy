fa18286882f84fcc1ce2dee9f7db0a7d4c15b1d4
Yael Borger <yborger217@gmail.com>
2022-10-02 16:51:23 -0400

    Pop-up Pops Up

---
+++ pop-up.js
@@ -0,0 +1,15 @@
+//background.js file used to help create this, so we know what to credit
+
+chrome.runtime.onInstalled.addListener(async () => {
+//addListener is listening to what we want to do when this opens
+    let url = chrome.runtime.getURL("pop-up.html");
+    //open the html file with the name
+    let tab = await chrome.tabs.create({ url });
+    //the tab create command within chrome will return a tab that shows what is in the url (the html)
+    console.log(`Created tab ${tab.id}`);
+    //console log the ID of the tab so we know it's working
+});
+function surprise(){
+    alert("SURPRISE!\nIt works! Wooooooooooooo!");
+
+}
