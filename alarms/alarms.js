f24d647ead04be9ef46f5cc4ce28c28ed32dd53f
Wasay <wasayq05@outlook.com>
2022-10-17 00:07:39 -0400

    Block websites

--- alarms/alarms.js
+++ alarms/alarms.js
@@ -6,15 +6,21 @@ var reminder_time = prompt("How many minutes from now do you want to be reminded
 if (reminder_name == null || reminder_name == "" || reminder_time == null || reminder_time == ""){
   text = "The reminder was cancelled";
 }
+
 else{
   text = "Reminder " + reminder_name + " set for " + reminder_time + " minutes from now.";
+<<<<<<< Updated upstream:alarms/alarms.js
   document.getElementById("new reminder").innerHTML = text;
   chrome.alarms.create(reminder_name, {delayInMinutes: parseInt(reminder_time), } );
+=======
+
+  chrome.alarms.create(reminder_name, {delayInMinutes : parseInt(reminder_time)} );
+  document.getElementById("new reminder").innerHTML = text;
+>>>>>>> Stashed changes:alarms.js
 
   chrome.alarms.onAlarm.addListener(function(alarm){
     console.log(alarm)
   });
 }
-
 document.getElementById("new reminder").innerHTML = text;
 }
