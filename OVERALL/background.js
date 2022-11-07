1e04eb7f6a0a04230c85f50d8188b7786fdcc7cc
Yael Borger <yborger217@gmail.com>
2022-11-07 16:27:37 -0500

    website blocked

--- OVERALL/background.js
+++ OVERALL/background.js
@@ -4,12 +4,24 @@ chrome.runtime.onInstalled.addListener(async() => {
     console.log(`Created tab ${tab.id}`);
     });
 
-chrome.runtime.onStartup.addListener("pageshow", urlRead())
-
-
-async function urlRead(){
-    let checkURL = chrome.runtime.getURL();
-    if(checkURL.includes("instagram")){
-
+//pre-writing some code: for blocking feature we're going to use chrome.storage I assume
+//this gets that storage when it is changed
+let blockingList = ['instagram', 'facebook'];
+chrome.storage.onChanged.addListener((changes, area) => {
+    if(area === 'sync' && changes.options?.newValue){
+        console.log("something has changed");
     }
-}
+
+});
+
+//when url in a tab changes this is activated
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){ 
+    console.log(tab.url);
+    checkURL = tab.url.toLowerCase()
+    for(let i = 0; i < blockingList.length; i++){
+        if(checkURL.includes(blockingList[i])){
+        let oops = chrome.runtime.getURL('../Images/Phox_copy.jpg')
+        chrome.tabs.update(null, {url: oops});
+        }
+    }
+})
