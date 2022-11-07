5bbab7726ae026a5903ba458841857ccc707445b
Alyssa Zhang <alyssazhang@dhcp-130-58-160-52.swarthmore.edu>
2022-11-07 16:25:45 -0500

    finishing buddy choice page UI

--- OVERALL/buddy_choice.js
+++ OVERALL/buddy_choice.js
@@ -1,9 +1,28 @@
+let buddyChoice = ''
+//variable for testing
+//let user_choice = document.querySelector('#user_choice')
+
 //For the Phrog button
 phrogChoice = document.getElementById('phrog')
-phrogChoice.onclick = () => {console.log('phrog')}
+//phrogChoice.onclick = () => {console.log('phrog')}
+document.querySelector('#phrog').onclick = function () {
+    buddyChoice = 'phrog';
+}
 //For the Phox button
 phoxChoice = document.getElementById('phox')
-phoxChoice.onclick = () => {console.log('phox')}
+//phoxChoice.onclick = () => {console.log('phox')}
+document.querySelector('#phox').onclick = function () {
+    buddyChoice = 'phox';
+}
 //For the Phlamingo button
 phlamingoChoice = document.getElementById('phlamingo')
-phlamingoChoice.onclick = () => {console.log('phlamingo')}
+//phlamingoChoice.onclick = () => {console.log('phlamingo')}
+document.querySelector('#phlamingo').onclick = function () {
+    buddyChoice = 'phlamingo';
+}
+
+//Button action for testing user input storage
+/*
+user_choice.addEventListener('click', function() {
+    console.log(buddyChoice)
+}) */
