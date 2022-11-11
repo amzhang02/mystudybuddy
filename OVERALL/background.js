0fece42f7a2224ebee9ad8bbddf821dbaab28fc3
Yael Borger <yborger217@gmail.com>
2022-11-11 13:47:11 -0500

    working on putting the alarms into bg NOT DONE

--- OVERALL/background.js
+++ OVERALL/background.js
@@ -4,15 +4,7 @@ chrome.runtime.onInstalled.addListener(async() => {
     console.log(`Created tab ${tab.id}`);
     });
 
-//pre-writing some code: for blocking feature we're going to use chrome.storage I assume
-//this gets that storage when it is changed
-let blockingList = ['instagram', 'facebook'];
-chrome.storage.onChanged.addListener((changes, area) => {
-    if(area === 'sync' && changes.options?.newValue){
-        console.log("something has changed");
-    }
-
-});
+let blockingList = ["instagram", "facebook"];
 
 //when url in a tab changes this is activated
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){ 
@@ -24,4 +16,49 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
         chrome.tabs.update(null, {url: oops});
         }
     }
-})
+});
+
+//alarms! woo!
+chrome.alarms.onAlarm.addListener((alarm) =>{
+    alert(alarm.name);
+});
+
+
+
+//reminder form
+function openReminderForm() {
+    var reminder_name = prompt("What do you want to name the reminder?");
+    var reminder_time = prompt("How many minutes from now do you want to be reminded?");
+    if (reminder_name == null || reminder_name == "" || reminder_time == null || reminder_time == ""){
+      text = "The reminder was cancelled";
+    }
+    else{
+      text = "Reminder '" + reminder_name + "'" + "set for " + reminder_time + " minutes from now.";
+      user_reminders.push(reminder_name);
+      chrome.alarms.create(reminder_name, {delayInMinutes : parseInt(reminder_time)} );
+    }
+    //document.getElementById("new reminder set").innerHTML = text;
+  }
+
+  function generalReminderForm(){
+    text = "You will now occasionally get reminders to stretch, drink water, and take a break from studying.";
+    chrome.alarms.create("drink water", {periodInMinutes : parseInt(1)} );
+    chrome.alarms.create("stretch", {periodInMinutes : parseInt(2)} );
+    chrome.alarms.create("take a break", {periodInMinutes : parseInt(3)} );
+    //document.getElementById("new reminder set").innerHTML = text;
+};
+
+
+  function clearReminders(){
+    chrome.alarms.clearAll();
+    text = "All reminders cleared.";
+    //document.getElementById("new reminder set").innerHTML = text;
+  }
+
+  function cleargenReminders(){
+    chrome.alarms.clear("drink water");
+    chrome.alarms.clear("strech");
+    chrome.alarms.clear("take a break");
+    text = "General reminders stopped.";
+    //document.getElementById("new reminder set").innerHTML = text;
+  }
--- OVERALL/popup.js
+++ OVERALL/popup.js
@@ -8,83 +8,84 @@ chrome.runtime.onInstalled.addListener(async () => {
         //the tab create command within chrome will return a tab that shows what is in the url (the html)
         console.log(`Created tab ${tab.id}`);
         //console log the ID of the tab so we know it's working
+});
+
+const button1 = document.getElementById("button1");
+console.log("test: ", button1);
+button1.addEventListener("click", clickyclick);
+
+function clickyclick(){
+    //document.body.style.backgroundColor = 'pink';
+    console.log("click");
+    chrome.tabs.create({url: "ext_tab.html"});
+}
+
+let user_reminders = [];
+
+//commented out to test background.js
+/*function openReminderForm() {
+    var reminder_name = prompt("What do you want to name the reminder?");
+    var reminder_time = prompt("How many minutes from now do you want to be reminded?");
+  
+    if (reminder_name == null || reminder_name == "" || reminder_time == null || reminder_time == ""){
+      text = "The reminder was cancelled";
+    }
+    else{
+      text = "Reminder '" + reminder_name + "'" + "set for " + reminder_time + " minutes from now.";
+
+      user_reminders.push(reminder_name);
+      
+      chrome.alarms.create(reminder_name, {delayInMinutes : parseInt(reminder_time)} );
+  
+      chrome.alarms.onAlarm.addListener(function(alarm) {
+        console.log(alarm);
+        alert(alarm.name);
+      });
+    }
+    document.getElementById("new reminder set").innerHTML = text;
+  }
+
+  function generalReminderForm(){
+    text = "You will now occasionally get reminders to stretch, drink water, and take a break from studying.";
+
+    chrome.alarms.create("drink water", {periodInMinutes : parseInt(1)} );
+    chrome.alarms.create("stretch", {periodInMinutes : parseInt(2)} );
+    chrome.alarms.create("take a break", {periodInMinutes : parseInt(3)} );
+
+    chrome.alarms.onAlarm.addListener(function(alarm) {
+      console.log(alarm);
+      alert(alarm.name);
     });
-    
-    const button1 = document.getElementById("button1");
-    console.log("test: ", button1);
-    button1.addEventListener("click", changeColor);
-    
-    function changeColor(){
-        //document.body.style.backgroundColor = 'pink';
-        console.log("click");
-        chrome.tabs.create({url: "ext_tab.html"});
-    }
 
-    let user_reminders = [];
+    document.getElementById("new reminder set").innerHTML = text;
+  }
 
-    function openReminderForm() {
-        var reminder_name = prompt("What do you want to name the reminder?");
-        var reminder_time = prompt("How many minutes from now do you want to be reminded?");
-      
-        if (reminder_name == null || reminder_name == "" || reminder_time == null || reminder_time == ""){
-          text = "The reminder was cancelled";
-        }
-        else{
-          text = "Reminder '" + reminder_name + "'" + "set for " + reminder_time + " minutes from now.";
+  function clearReminders(){
+    chrome.alarms.clearAll();
+    text = "All reminders cleared.";
+    document.getElementById("new reminder set").innerHTML = text;
+  }
 
-          user_reminders.push(reminder_name);
-          
-          chrome.alarms.create(reminder_name, {delayInMinutes : parseInt(reminder_time)} );
-      
-          chrome.alarms.onAlarm.addListener(function(alarm) {
-            console.log(alarm);
-            alert(alarm.name);
-          });
-        }
-        document.getElementById("new reminder set").innerHTML = text;
-      }
-    
-      function generalReminderForm(){
-        text = "You will now occasionally get reminders to stretch, drink water, and take a break from studying.";
+  function cleargenReminders(){
+    chrome.alarms.clear("drink water");
+    chrome.alarms.clear("strech");
+    chrome.alarms.clear("take a break");
+    text = "General reminders stopped.";
+    document.getElementById("new reminder set").innerHTML = text;
+  }
+*/
+var port = chrome.extension.connect({
+  name: "Checking..."
+});
 
-        chrome.alarms.create("drink water", {periodInMinutes : parseInt(1)} );
-        chrome.alarms.create("stretch", {periodInMinutes : parseInt(2)} );
-        chrome.alarms.create("take a break", {periodInMinutes : parseInt(3)} );
+document.getElementById("reminderbtn").addEventListener("click", sending("reminder"));
+document.getElementById("genreminderbtn").addEventListener("click", sending("genReminder"));
+//document.getElementById("remindersclear").addEventListener("click", sending("clearReminders"));
+//document.getElementById("genremindersclear").addEventListener("click", sending("cleargenReminders"));
 
-        chrome.alarms.onAlarm.addListener(function(alarm) {
-          console.log(alarm);
-          alert(alarm.name);
-        });
-
-        document.getElementById("new reminder set").innerHTML = text;
-      }
-
-      function clearReminders(){
-        chrome.alarms.clearAll();
-        text = "All reminders cleared.";
-        document.getElementById("new reminder set").innerHTML = text;
-      }
-
-      function cleargenReminders(){
-        chrome.alarms.clear("drink water");
-        chrome.alarms.clear("strech");
-        chrome.alarms.clear("take a break");
-        text = "General reminders stopped.";
-        document.getElementById("new reminder set").innerHTML = text;
-      }
-    /*function surprise(){
-        chrome.notifications.create({
-            notificationsId? ,
-            options: 
-            callback?:function,
-        });
-    }
-    
-    function openTab(){
-        chrome.tabs.create({});
-    }*/
-    
-    document.getElementById("reminderbtn").addEventListener("click", openReminderForm);
-    document.getElementById("genreminderbtn").addEventListener("click", generalReminderForm);
-    document.getElementById("remindersclear").addEventListener("click", clearReminders);
-    document.getElementById("genremindersclear").addEventListener("click", cleargenReminders);
+function sending(message){
+  port.postMessage({text: message})
+  port.onMessage.addListener(function(msg){
+    console.log(msg);
+  })
+}
