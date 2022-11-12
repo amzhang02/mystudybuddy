03806a04863ba2850f456919e69eb923422db035
Alyssa Zhang <alyssazhang@dhcp-130-58-160-52.swarthmore.edu>
2022-11-12 14:29:52 -0500

    finished UI and figured out how to store user variables locally

--- OVERALL/buddy_choice.js
+++ OVERALL/buddy_choice.js
@@ -7,18 +7,21 @@ phrogChoice = document.getElementById('phrog')
 //phrogChoice.onclick = () => {console.log('phrog')}
 document.querySelector('#phrog').onclick = function () {
     buddyChoice = 'phrog';
+    location.replace('reminder_choice.html')
 }
 //For the Phox button
 phoxChoice = document.getElementById('phox')
 //phoxChoice.onclick = () => {console.log('phox')}
 document.querySelector('#phox').onclick = function () {
     buddyChoice = 'phox';
+    location.replace('reminder_choice.html')
 }
 //For the Phlamingo button
 phlamingoChoice = document.getElementById('phlamingo')
 //phlamingoChoice.onclick = () => {console.log('phlamingo')}
 document.querySelector('#phlamingo').onclick = function () {
     buddyChoice = 'phlamingo';
+    location.replace('reminder_choice.html')
 }
 
 //Button action for testing user input storage
