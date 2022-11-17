3c855191646db4ed7fa22109bf2e455f359c696e
Alyssa Zhang <alyssazhang@dhcp-130-58-160-52.swarthmore.edu>
2022-11-17 01:00:25 -0500

    added single-choice borders to buddy choice

--- OVERALL/buddy_choice.js
+++ OVERALL/buddy_choice.js
@@ -1,31 +1,57 @@
-let buddyChoice = ''
+let phrogClicked = false;
+let phoxClicked = false;
+let phlamingoClicked = false;
 //variable for testing
 //let user_choice = document.querySelector('#user_choice')
 
-//For the Phrog button
-phrogChoice = document.getElementById('phrog')
-//phrogChoice.onclick = () => {console.log('phrog')}
-document.querySelector('#phrog').onclick = function () {
-    buddyChoice = 'phrog';
-    location.replace('reminder_choice.html?buddy=phrog')
-}
-//For the Phox button
-phoxChoice = document.getElementById('phox')
-//phoxChoice.onclick = () => {console.log('phox')}
-document.querySelector('#phox').onclick = function () {
-    buddyChoice = 'phox';
-    location.replace('reminder_choice.html?buddy=phox')
-}
-//For the Phlamingo button
-phlamingoChoice = document.getElementById('phlamingo')
-//phlamingoChoice.onclick = () => {console.log('phlamingo')}
-document.querySelector('#phlamingo').onclick = function () {
-    buddyChoice = 'phlamingo';
-    location.replace('reminder_choice.html?buddy=phlamingo')
+document.querySelector('#nextButton').onclick = function () {
+    if(phrogClicked) {
+        location.replace('reminder_choice.html?buddy=phrog');
+    } else if(phoxClicked) {
+        location.replace('reminder_choice.html?buddy=phox');
+    } else if(phlamingoClicked) {
+        location.replace('reminder_choice.html?buddy=phlamingo');
+    } else {
+        alert("You must select a buddy to continue!");
+    }
 }
 
-//Button action for testing user input storage
-/*
-user_choice.addEventListener('click', function() {
-    console.log(buddyChoice)
-}) */
+//For the Phrog button
+document.querySelector('#phrog').onclick = function () {
+    phrogClicked = !phrogClicked;
+    if(phrogClicked) {
+        this.style.border = "1px solid blue";
+        phox.style.border = "";
+        phlamingo.style.border = "";
+        phoxClicked = false;
+        phlamingoClicked = false;
+    } else {
+        this.style.border = "";
+    }
+}
+//For the Phox button
+document.querySelector('#phox').onclick = function () {
+    phoxClicked = !phoxClicked;
+    if(phoxClicked) {
+        this.style.border = "1px solid blue";
+        phrog.style.border = "";
+        phlamingo.style.border = "";
+        phrogClicked = false;
+        phlamingoClicked = false;
+    } else {
+        this.style.border = "";
+    }
+}
+//For the Phlamingo button
+document.querySelector('#phlamingo').onclick = function () {
+    phlamingoClicked = !phlamingoClicked;
+    if(phlamingoClicked) {
+        this.style.border = "1px solid blue";
+        phrog.style.border = "";
+        phox.style.border = "";
+        phrogClicked = false;
+        phoxClicked = false;
+    } else {
+        this.style.border = "";
+    }
+}
